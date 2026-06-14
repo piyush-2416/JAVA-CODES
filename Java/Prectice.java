6d5edf5f520dd12807f15f8bb455e612f0c5f832
piyush verma <www.pankajverma.981139@gmail.com>
2026-06-14 23:47:52 +0530

    Update Prectice.java

--- Java/Prectice.java
+++ Java/Prectice.java
@@ -4,14 +4,8 @@ public class Prectice {
 public static void main(String[] args) {
     String str="hello java programming";
 String name="shagun  verma";
-String name2="jimmi verma";
-
-
-String rev="";
-for(int i=0; i<str.length()-1;i++){
-    System.out.println(str(i));
-    rev +="";
-}
+for(int i=0;i<str.length;i++){
+    system.out
 
 
 }
