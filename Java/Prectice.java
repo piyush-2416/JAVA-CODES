23bc33f47d7468e5e1e3a168307cd9e9331f3a62
piyush-2416 <pankajverma.981139@gmail.com>
2026-06-15 23:10:33 +0530

    neww changes in code

--- Java/Prectice.java
+++ Java/Prectice.java
@@ -2,20 +2,10 @@ public class Prectice {
     // Online Java Compiler
 // Use this editor to write, compile and run your Java code online
 public static void main(String[] args) {
-<<<<<<< HEAD
-    int a = 10;
-    int b = 20;
-    int c = a + b;
-    System.out.println("The sum of a and b is: " + c);
-=======
-    String str="hello java programming";
-String name="shagun  verma";
-for(int i=0;i<str.length;i++){
-    system.out
->>>>>>> 6d5edf5f520dd12807f15f8bb455e612f0c5f832
 
 
+StringBuilder sb = new StringBuilder("Hello");
+sb.reverse();
+System.out.println(sb);
 }
-
-
 }
