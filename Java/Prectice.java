3a1209d56c0fee92bb58a1965e730dfddfeb7073
piyush-2416 <pankajverma.981139@gmail.com>
2026-06-23 23:07:15 +0530

    Learn the Arryas

--- Java/Prectice.java
+++ Java/Prectice.java
@@ -2,21 +2,23 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Prectice {
-    // Online Java Compiler
-// Use this editor to write, compile and run your Java code online
-
+   
     public static void main(String[] args) {
 
-  String str="anagram";
-  int start=0;
-  int end=str.length()-1;
-  for(int i=end;i<start;i++){
-    if(start.charAt(i)==end.charAt(i)){
-        System.out.println("anagrams");
-    }else{
-        System.out.println("No");
-    }
-  }
-       
+        int[] arr = {10, 20, 30, 40, 50};
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+
+        System.out.println(Arrays.toString(arr));
     }
 }
