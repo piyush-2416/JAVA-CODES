2e011c94215cc2e1a92267ea48fc55e0484ebd9d
piyush verma <www.pankajverma.981139@gmail.com>
2026-07-10 12:40:27 +0530

    Prectice on Arrays (1d & 2d) problems like kids with greatest number of candies
    Refactored main method to find and compare elements in an array.

--- Java/Prectice.java
+++ Java/Prectice.java
@@ -2,24 +2,32 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Prectice {
-public static void main(String[] args) {  
-   int[][]arr={{1,2,3},{3,2,1}};
-   int sum1=0;
-   int sum2=0;
-   int max=0;
-   for(int i=0;i<arr.length;i++){
-  for(int j=0;j<arr[i].length;j++){
-   System.out.println(Arrays.toString(arr[j])); 
+    public static void main(String[] args) {
+        int[] arr = { 1, 7, 5, 1, 3 };
+        int largest = arr[0];
+        int extra = 3;
+        boolean[] newarr = new boolean[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];}
 
-  }   
+            }
+            for(int i=0;i<arr.length;i++){
 
- 
-   }
-}
-          
-      
- 
-}
+            
+                int Add = arr[i] + extra;
 
- 
+                if (Add >= largest) {
+                    System.out.println(newarr[i] = true );
+                    System.out.println(arr[i]);
+
+                }else if(Add<largest){
+                    newarr[i]=false;
+                    System.out.println(newarr[i]);
+                    System.out.println(arr[i]);
+                }
+            }
+        }
+
+    }
 
